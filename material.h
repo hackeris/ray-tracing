84734ae098d95f329d917a27a2f832e61423dd67
hackeris <hackeris@qq.com>
2022-02-25 23:54:24 +0800

    refraction

--- material.h
+++ material.h
@@ -20,7 +20,7 @@ public:
 
 class lambertian : public material {
 public:
-    lambertian(const color &a) : albedo(a) {}
+    explicit lambertian(const color &a) : albedo(a) {}
 
     bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override;
 
@@ -43,20 +43,53 @@ bool lambertian::scatter(const ray &r_in, const hit_record &rec, color &attenuat
 
 class metal : public material {
 public:
-    metal(const color &a) : albedo(a) {}
+    explicit metal(const color &a, double f) : albedo(a), fuzz(f) {}
 
     bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override;
 
 public:
     color albedo;
+    double fuzz;
 };
 
 bool metal::scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const {
 
     vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-    scattered = ray(rec.p, reflected);
+    scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
     attenuation = albedo;
     return (dot(scattered.direction(), rec.normal) > 0);
+}
+
+class dielectric : public material {
+public:
+    explicit dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+    bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override;
+
+public:
+    double ir;
+};
+
+bool dielectric::scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const {
+
+    attenuation = color(1.0, 1.0, 1.0);
+
+    double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
+
+    vec3 unit_direction = unit_vector(r_in.direction());
+    double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+    double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+    bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+    vec3 direction;
+    if (cannot_refract) {
+        direction = reflect(unit_direction, rec.normal);
+    } else {
+        direction = refract(unit_direction, rec.normal, refraction_ratio);
+    }
+
+    scattered = ray(rec.p, direction);
+    return true;
 }
 
 #endif //RAY_TRACING_MATERIAL_H
