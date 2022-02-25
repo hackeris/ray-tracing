aae0182ae2d1cb411a750bce9d35be32572200a3
hackeris <hackeris@qq.com>
2022-02-26 00:10:44 +0800

    full glass

--- material.h
+++ material.h
@@ -66,6 +66,13 @@ public:
 
     bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override;
 
+private:
+    static double reflectance(double cosine, double ref_idx) {
+        auto r0 = (1 - ref_idx) / (1 + ref_idx);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * pow(1 - cosine, 5);
+    }
+
 public:
     double ir;
 };
@@ -82,7 +89,7 @@ bool dielectric::scatter(const ray &r_in, const hit_record &rec, color &attenuat
 
     bool cannot_refract = refraction_ratio * sin_theta > 1.0;
     vec3 direction;
-    if (cannot_refract) {
+    if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double()) {
         direction = reflect(unit_direction, rec.normal);
     } else {
         direction = refract(unit_direction, rec.normal, refraction_ratio);
