ca488e58d486c75d10e879068b9946063dd095da
hackeris <hackeris@qq.com>
2022-02-26 00:22:02 +0800

    positioning/orienting camera

--- main.cpp
+++ main.cpp
@@ -44,7 +44,7 @@ int main() {
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left = make_shared<dielectric>(1.5);
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.2);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
@@ -53,7 +53,7 @@ int main() {
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     //  camera
-    camera cam;
+    camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 20, aspect_ratio);
 
     //  render
     std::cout << "P3" << std::endl << image_width << ' ' << image_height << "\n255\n";
